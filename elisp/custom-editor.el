40bb53398ed0aebb2f5c2dee012b799720f29c5e
Rand01ph <tanyawei1991@gmail.com>
2018-08-09 20:05:42 +0800

    增加md支持

--- elisp/custom-editor.el
+++ elisp/custom-editor.el
@@ -51,5 +51,13 @@
     (setq undo-tree-visualizer-timestamps t)
     (setq undo-tree-visualizer-diff t))) 
 
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
 (provide 'custom-editor)
 ;;; custom-editor.el ends
