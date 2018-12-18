76eb7b7fb87e0cbce912f3a3430a0253471852f7
Rand01ph <tanyawei1991@gmail.com>
2018-12-18 19:19:59 +0800

    update org blog

--- elisp/custom-org.el
+++ elisp/custom-org.el
@@ -3,25 +3,32 @@
 (use-package htmlize
   :ensure t)
 
-
 (use-package org
   :config
+  (defun org-publish-sitemap-time-entry (entry style project)
+    (format "%s %s"
+	    (format-time-string
+	     "[%Y-%m-%d %a %H:%s]"
+	     (org-publish-find-date entry project))
+	    (org-publish-sitemap-default-entry entry style project)))
+
   (setq org-publish-project-alist
 	'(("blog"
 	   :components ("blog-content" "blog-static"))
 	  ("blog-content"
-	   :base-directory "~/Projects/Rand01ph.github.io/org"
+	   :base-directory "~/Projects/OrgNote/Blog"
 	   :base-extension "org" ;扩展名
 	   :publishing-directory "~/Projects/Rand01ph.github.io"
 	   :recursive t
 	   :publishing-function (org-html-publish-to-html)
 	   :headline-levels 4
-           :section-numbers nil
+	   :section-numbers nil
 	   :auto-preamble t
 	   :with-toc t
-	   
-	   :sitemap-file-entry-format "%d - %t"
-	   :sitemap-date-format "%d.%m.%Y"
+
+	   :sitemap-format-entry org-publish-sitemap-time-entry
+	   :sitemap-date-format "%Y-%m-%d"
+	   :sitemap-file-entry-format "%d *%t*"
 	   :sitemap-sort-files anti-chronologically
 	   :sitemap-filename "index.org"  ; ... call it sitemap.org (it's the default)...
 	   :sitemap-title "Just for fun"         ; ... with title 'Sitemap'.
@@ -33,12 +40,13 @@
 	   :html-link-up "index.html"
 
 	   :author "Rand01ph"
-	   :email "tanyawei1991 at gmail dot com"
-           :language "zh-CN"
+	   :email "tanyawei1991@gmail.com"
+	   :language "zh-CN"
 	   :html-head-extra
 	   "<link rel=\"stylesheet\" type=\"text/css\" href=\"https://gongzhitaao.org/orgcss/org.css\"/>")
+
 	  ("blog-static"
-	   :base-directory "~/Projects/Rand01ph.github.io/org/static"
+	   :base-directory "~/Projects/OrgNote/Blog/static"
 	   :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf"
 	   :publishing-directory "~/Projects/Rand01ph.github.io/static/"
 	   :recursive t
